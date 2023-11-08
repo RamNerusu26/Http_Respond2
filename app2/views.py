1560efa3a93296d09a2e366d40655821554f9419
RamNerusu26 <ramnerusu08@gmail.com>
2023-11-08 11:02:47 +0530

    first commit

---
+++ app2/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def nerusu(request):
+    return HttpResponse('<h1><marquee>||JAI SHREE RAM JAI SHREE RAM * RAJA RAM *||</h1></marquee>')
